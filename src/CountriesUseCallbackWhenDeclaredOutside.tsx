46ac5eca112e0ac8fdd6e0dece5aa349576e53de
heriberto perez magaña <joseheribertoperezmagana@gmail.com>
2024-02-06 00:13:55 -0600

    Quick example using style component element

--- src/CountriesUseCallbackWhenDeclaredOutside.tsx
+++ src/CountriesUseCallbackWhenDeclaredOutside.tsx
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect, useCallback, PointerEvent } from 'react';
 import Select from '@mui/material/Select';
 import MenuItem from '@mui/material/MenuItem';
-import { SelectChangeEvent } from '@mui/material';
+import { FormControl, InputLabel, SelectChangeEvent } from '@mui/material';
+import styled from 'styled-components';
 
 // searching all countries
 // path: /all
@@ -20,15 +21,28 @@ interface CountryCardProps {
   country: Country
 }
 
-const CountryCard: React.FC<CountryCardProps> = ({ country }) => {
+// Same definition as below but using React.FC or React.FunctionComponent
+// as this is more tied to React functional components and with the type
+// both are valid and probably specific to preferences or other requirements
+// const CountryCard: React.FC<CountryCardProps> = ({ country }) => {
+const CountryCard: React.FunctionComponent<CountryCardProps> = ({ country }) => {
   return (
     <h3>{country.name.common}</h3>
   )
 }
 
+// second approach using Direct props typing
+// const CountryCard = ({ country }: CountryCardProps) => {
+//   return (
+//     <h3>{country.name.common}</h3>
+//   )
+// }
+
+
 const BASE_URL: string = 'https://restcountries.com/v3.1';
 const FILTERABLE_CAPITALS = [
   'All',
+  'México',
   'Tallin',
   'Helsinki',
   'Stockholm',
@@ -49,22 +63,52 @@ const CountriesUseCallbackWhenDeclaredOutside = () => {
     setSelectedCapital(event.target.value as Capital);
   }
 
-  const fetchCountries = useCallback(async (callbackFunc: (countries: Country[]) => void) => {
+  const retrieveCountries = async (callbackFunc: (countries: Country[]) => void) => {
     const fetchUrl = selectedCapital === 'All' ? '/all' : `/capital/${selectedCapital}`;
     const fetchAllUrl = `${BASE_URL}${fetchUrl}`;
     const fetchRequest = await fetch(fetchAllUrl);
     const fetchedData = await fetchRequest.json();
     callbackFunc(fetchedData);
-  }, [selectedCapital])
+  }
+
+  const fetchCountries = useCallback(retrieveCountries, [selectedCapital])
+  const [age, setAge] = useState<string>('');
+
+  const handleSelectAgeChange = (event: PointerEvent) => {
+    console.log(event, 'event')
+  }
 
   const renderCapitalSelectOptions = () => (
-    <Select
-      value={selectedCapital}
-      onChange={handleSelectChange}
-      labelId="capitals">
-      {FILTERABLE_CAPITALS.map((capital) => <MenuItem key={capital} value={capital} >{capital}</MenuItem>)}
-    </Select>
+    <>
+      <FormControl sx={{ m: 1, minWidth: 120 }}>
+        <InputLabel id="demo-simple-select-standard-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-standard-label"
+          id="demo-simple-select-standard"
+          value={age}
+          onChange={handleSelectAgeChange}
+          label="Age"
+        >
+          <MenuItem value="">
+            <em>None</em>
+          </MenuItem>
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      <Select
+        value={selectedCapital}
+        onChange={handleSelectChange}
+        labelId="capitals">
+        {FILTERABLE_CAPITALS.map((capital) => <MenuItem key={capital} value={capital} >{capital}</MenuItem>)}
+      </Select>
+    </>
   )
+
+  const Description = styled.h2`
+    color: green;
+  `
 
   useEffect(() => {
     fetchCountries(setCountries);
@@ -72,7 +116,8 @@ const CountriesUseCallbackWhenDeclaredOutside = () => {
 
   return (
     <>
-      <h1>List of Countries - CountriesUseCallbackWhenDeclaredOutside </h1>
+      <h1>List of Countries</h1>
+      <Description>CountriesUseCallbackWhenDeclaredOutside</Description>
       {renderCapitalSelectOptions()}
       {countries.map(country => <CountryCard key={country.name.common} country={country} />)}
     </>
